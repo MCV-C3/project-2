bd48759758aeeb06afd09e5723ee3316142db1d4
Xavier Pacheco Bach <xavipba@gmail.com>
2025-12-03 20:23:20 +0100

    Initial commit xavi branch

---
+++ Week1/parameters.py
@@ -0,0 +1,19 @@
+# Classifier parameters
+CLASSIFIER_PARAMETERS = {
+    "LogisticRegression": {
+        "penalty": "l2",
+        "C": 1.0,
+    },
+    "SVM": {
+        "kernel": "rbf",
+        "C": 1.0,
+        "gamma": "scale"
+    }
+}
+CLASSIFIER = "LogisticRegression"
+
+# Detector parameters
+DETECTOR_PARAMETERS = {
+}
+
+# Codebook parameters
