4a1ee19c2d6f6e5036ed8a2a6086f98a0dd2767b
Pol Riubrogent <pol.riubrogent@autonoma.cat>
2025-12-04 16:51:03 +0100

    Spatial pyramid code

--- Week1/bovw.py
+++ Week1/bovw.py
@@ -9,9 +9,9 @@ import glob
 from typing import *
 
 class BOVW():
-    
-    def __init__(self, detector_type="AKAZE", codebook_size:int=50, detector_kwargs:dict={}, codebook_kwargs:dict={}):
 
+    def __init__(self, detector_type="AKAZE", codebook_size:int=50, detector_kwargs:dict={}, codebook_kwargs:dict={},
+                 spatial_pyramid:str=None, pyramid_levels:int=1):
         if detector_type == 'SIFT':
             self.detector = cv2.SIFT_create(**detector_kwargs)
         elif detector_type == 'AKAZE':
@@ -20,9 +20,11 @@ class BOVW():
             self.detector = cv2.ORB_create(**detector_kwargs)
         else:
             raise ValueError("Detector type must be 'SIFT', 'SURF', or 'ORB'")
-        
+
         self.codebook_size = codebook_size
         self.codebook_algo = MiniBatchKMeans(n_clusters=self.codebook_size, **codebook_kwargs)
+        self.spatial_pyramid = spatial_pyramid
+        self.pyramid_levels = pyramid_levels
         
                
     ## Modify this function in order to be able to create a dense sift
@@ -43,17 +45,96 @@ class BOVW():
     def _compute_codebook_descriptor(self, descriptors: Literal["1 T d"], kmeans: Type[KMeans]) -> np.ndarray:
 
         visual_words = kmeans.predict(descriptors)
-        
-        
+
+
         # Create a histogram of visual words
         codebook_descriptor = np.zeros(kmeans.n_clusters)
         for label in visual_words:
             codebook_descriptor[label] += 1
-        
+
         # Normalize the histogram (optional)
-        codebook_descriptor = codebook_descriptor / np.linalg.norm(codebook_descriptor)
-        
-        return codebook_descriptor       
+        if np.linalg.norm(codebook_descriptor) > 0:
+            codebook_descriptor = codebook_descriptor / np.linalg.norm(codebook_descriptor)
+
+        return codebook_descriptor
+
+    def _extract_features_with_spatial_pyramid(self, image: Literal["H", "W", "C"]) -> Tuple[List, List]:
+        keypoints, descriptors = self._extract_features(image)
+        return keypoints, descriptors
+
+    def _get_spatial_regions(self, image_shape: Tuple[int, int]) -> List[Tuple[int, int, int, int]]:
+        h, w = image_shape
+        regions = []
+
+        if self.spatial_pyramid is None:
+            return [(0, 0, w, h)]
+
+        elif self.spatial_pyramid == 'horizontal':
+            # Divide image into horizontal strips
+            strip_height = h // self.pyramid_levels
+            for i in range(self.pyramid_levels):
+                y1 = i * strip_height
+                y2 = h if i == self.pyramid_levels - 1 else (i + 1) * strip_height
+                regions.append((0, y1, w, y2))
+
+        elif self.spatial_pyramid == 'vertical':
+            # Divide image into vertical strips
+            strip_width = w // self.pyramid_levels
+            for i in range(self.pyramid_levels):
+                x1 = i * strip_width
+                x2 = w if i == self.pyramid_levels - 1 else (i + 1) * strip_width
+                regions.append((x1, 0, x2, h))
+
+        elif self.spatial_pyramid == 'square':
+            # Divide image into square grid
+            strip_height = h // self.pyramid_levels
+            strip_width = w // self.pyramid_levels
+            for i in range(self.pyramid_levels):
+                for j in range(self.pyramid_levels):
+                    y1 = i * strip_height
+                    y2 = h if i == self.pyramid_levels - 1 else (i + 1) * strip_height
+                    x1 = j * strip_width
+                    x2 = w if j == self.pyramid_levels - 1 else (j + 1) * strip_width
+                    regions.append((x1, y1, x2, y2))
+
+        return regions
+
+    def _compute_spatial_pyramid_descriptor(self, image: Literal["H", "W", "C"], keypoints, descriptors, kmeans: Type[KMeans]) -> np.ndarray:
+        if descriptors is None or len(descriptors) == 0:
+            # Return zero vector if no descriptors
+            regions = self._get_spatial_regions(image.shape[:2])
+            return np.zeros(kmeans.n_clusters * len(regions))
+
+        h, w = image.shape[:2]
+        regions = self._get_spatial_regions((h, w))
+
+        pyramid_histogram = []
+
+        for (x1, y1, x2, y2) in regions:
+            # Find keypoints within this region
+            region_descriptors = []
+            for kp, desc in zip(keypoints, descriptors):
+                kp_x, kp_y = kp.pt
+                if x1 <= kp_x < x2 and y1 <= kp_y < y2:
+                    region_descriptors.append(desc)
+
+            # Compute histogram for this region
+            if len(region_descriptors) > 0:
+                region_descriptors = np.array(region_descriptors)
+                region_histogram = self._compute_codebook_descriptor(region_descriptors, kmeans)
+            else:
+                region_histogram = np.zeros(kmeans.n_clusters)
+
+            pyramid_histogram.append(region_histogram)
+
+        # Concatenate all region histograms
+        pyramid_histogram = np.concatenate(pyramid_histogram)
+
+        # Normalize the final concatenated histogram
+        if np.linalg.norm(pyramid_histogram) > 0:
+            pyramid_histogram = pyramid_histogram / np.linalg.norm(pyramid_histogram)
+
+        return pyramid_histogram       
     
 
 
